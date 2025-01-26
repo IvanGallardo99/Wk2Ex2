3664063de6caea79d495ed3a8e3b6aed4f88f60f
IvanGallardo99 <igallardo@amarillocollege.com>
2025-01-25 23:27:14 -0600

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace Wk2Ex2
         {
             Console.WriteLine("Rectangle Area and Perimeter Calculator\n");
 
-            // Input: Length of the rectangle
+            // Enter Length of the rectangle
             Console.Write("Enter the length of the rectangle: ");
             double length = Convert.ToDouble(Console.ReadLine());
 
-            // Input: Width of the rectangle
+            // Enter Width of the rectangle
             Console.Write("Enter the width of the rectangle: ");
             double width = Convert.ToDouble(Console.ReadLine());
 
